985cb7e2505cada932b77cfcf329a1c9e169bbd7
michaelthr33 <MStaunton@ucvts.tec.nj.us>
2019-11-06 10:47:25 -0500

    deleted gpa0

--- src/gpa0.js
+++
@@ -1,7 +0,0 @@
-const readlineSync = require("readline-sync");
-const GPA = readlineSync.question("Enter a letter grade:  ")
-const A = 4.00
-const B = 3.00
-const C = 2.00
-const D = 1.00
-const F = 0.00
