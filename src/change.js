027932ccd90bdfc807f40eccb3d7a37b5ff4c337
michaelthr33 <MStaunton@ucvts.tec.nj.us>
2019-11-06 10:45:48 -0500

    formatted

--- src/change.js
+++ src/change.js
@@ -9,19 +9,25 @@ const penny= .01;
 let pennynumber=0;
 const MIN = 0.01;
 const MAX = 0.99;
-
 let amount = Number(readlineSync.question("\nEnter a dollar amount: "));
-
-if (Number.isNaN(amount)) {console.log("Invalid")
-    // print your error message here
+if (Number.isNaN(amount)) {console.log("Invalid")    // print your error message here
 } else if (amount < MIN || amount > MAX) {console.log("Invalid")
-    // print your error message here
-} else if (amount >= penny){while(amount>=quarter){amount=amount-quarter;
-quarternumber++;}
-while(amount>=dime&amount<quarter){amount=amount-dime;
-dimenumber++;}
-while(amount>=nickel&amount<dime){amount=amount-nickel;
-nickelnumber++;}
-while(amount>=penny&amount<nickel){amount=amount-penny;
-pennynumber++;}}
-{console.log(""+quarternumber+" quarters, "+ dimenumber + " dimes, "+ nickelnumber +" nickels, and " + pennynumber +" pennies.");}
+  // print your error message here
+} else if (amount >= penny){
+    while(amount>=quarter){
+      amount=amount-quarter;
+      quarternumber++;
+    }
+    while(amount>=dime&amount<quarter){
+      amount=amount-dime;
+      dimenumber++;
+    }
+    while(amount>=nickel&amount<dime){
+      amount=amount-nickel;nickelnumber++;
+    }
+      while(amount>=penny&amount<nickel){
+        amount=amount-penny;
+        pennynumber++;
+  }
+}
+console.log(""+quarternumber+" quarters, "+ dimenumber + " dimes, "+ nickelnumber +" nickels, and " + pennynumber +" pennies.");
---
+++ src/temperature.js
@@ -0,0 +1,7 @@
+const readlineSync = require("readline-sync");
+const MAX=Number.MAX_SAFE_INTEGER;
+const MIN=Number.MIN_SAFE_INTEGER;
+const temperature = Number(readlineSync.question("Enter a temperature:  "))
+const scale =readlinesync.question("Enter a scale: ")
+const leapyearhundred = year%100;
+if (scale=="F"&temperature<=32){console.log("Liquid")}
