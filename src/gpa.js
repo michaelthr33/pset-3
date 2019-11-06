0b9c9445cef7648acac2d4d8f2899a311b3cc6e6
michaelthr33 <MStaunton@ucvts.tec.nj.us>
2019-11-06 10:16:37 -0500

    repaired output

--- src/gpa.js
+++ src/gpa.js
@@ -5,8 +5,8 @@ const B = 3.00
 const C = 2.00
 const D = 1.00
 const F = 0.00
-if (GPA=="A"){console.log("Your Gpa is a 4.00")}
-if (GPA=="B"){console.log("Your GPA is a 3.00")}
-if (GPA=="C"){console.log("Your GPA is a 2.00")}
-if (GPA=="D"){console.log("Your GPA is a 1.00")}
-if (GPA=="F"){console.log("Your GPA is a 0.00")}
+if (GPA=="A"){console.log("Your Gpa is 4.00")}
+if else (GPA=="B"){console.log("Your GPA is 3.00")}
+if else(GPA=="C"){console.log("Your GPA is 2.00")}
+if else(GPA=="D"){console.log("Your GPA is 1.00")}
+if (GPA=="F"){console.log("Your GPA is 0.00")}
